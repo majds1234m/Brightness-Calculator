5587bba45181587100d11e16252bba8d1d8b3538
majds1234m <m.al-attar1@share.epsb.ca>
2025-04-08 13:45:34 -0600

    Delete Brightness-Calculator directory

--- Brightness-Calculator/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn").addEventListener("click", onClicked)
-
-function onClicked() {
-let r = +document.getElementById("red").value;
-let g = +document.getElementById("green").value;
-let b = +document.getElementById("red").value;
-let answer = Math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2);
-document.getElementById("text").innerHTML = Math.round(answer);
-}
